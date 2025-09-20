43976b83fdb726178ac8b249adf9f98e2a430f3e
Joon Park <24joonpark2024@dhcp-10-105-62-114.wireless.northwestern.private>
2025-09-19 21:49:00 -0500

    Final

--- Dice Rolling.py
+++ Dice Rolling.py
@@ -1,17 +1,22 @@
 import random
 
 playing = True
+times = 0
 while playing: 
     choice = input("Roll the dice? (y/n): ").lower() #to always treat the result as lower case
     if choice == "y":
-        num_of_iterations = input("How many times? ")
-        n=0
-        while n <= int(num_of_iterations): 
-            die1 = random.randint(1,6)
-            print(f"({die1})")
-            n +=1 
+        num_of_iterations = (input("How many times? "))
+        n=1
+        if num_of_iterations == type(int):
+            print("Invalid Choice!")
+        else: 
+            while n <= int(num_of_iterations): 
+                die1 = random.randint(1,6)
+                print(f"({die1})")
+                n +=1 
+        times += int(num_of_iterations)
     elif choice == "n": 
-        print("Thank you for playing")
+        print(f"Thank you for playing! You have played {times} times!")
         break
     else: 
         print("Invalid Choice!")
